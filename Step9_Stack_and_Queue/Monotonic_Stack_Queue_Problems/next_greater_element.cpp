79503ce64615993dee98b583a531ffc57a601ef9
ParthKhiriya <b23ee1051@iitj.ac.in>
2025-07-23 10:24:12 +0530

    Completed the Next greater element question

---
+++ Step9_Stack_and_Queue/Monotonic_Stack_Queue_Problems/next_greater_element.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    vector<int> nextGreaterElements(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> nge(n, -1);
+        stack<int> st;
+
+        for(int i=2*n-1; i>=0; i--) {
+            while(!st.empty() && st.top() <= arr[i%n]) {
+                st.pop();
+            }
+            if(i < n) {
+                if(!st.empty()) nge[i] = st.top();
+            }
+            st.push(arr[i%n]);
+        } 
+
+        return nge;
+    }
+};
+
+int main () {
+    Solution obj;
+    vector < int > v {5,7,1,2,6,0};
+    vector < int > res = obj.nextGreaterElements(v);
+    cout << "The next greater elements are" << endl;
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";
+    }
+}
